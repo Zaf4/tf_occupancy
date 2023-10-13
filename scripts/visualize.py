a250db49a32fa06f9733544e4dd5bc7ffdb432e5
zaf4 <zafermolbio@gmail.com>
2023-10-13 03:31:28 +0300

    progress in slicing the chromosome

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -1,5 +1,6 @@
 import polars as pl
 import os
+import numpy as np
 from utility import timer
 
 #DEFINE globals
@@ -38,33 +39,59 @@ def draw_lines(chr:pl.DataFrame,template:str)->str:
     previous_end = 0
     line_count = 0
 
-    overlap_range = (0,1) #min,max
-
     for (start,),(width,) in zip(chr.select("Start").iter_rows(),chr.select("Range").iter_rows()):
-        
+        end = start+width
         #if no overlap, if -> (completely on left) and (completely on right)
-        if (start < previous_end) and (start+width>previous_start):
+        if (start < previous_end) and (end>previous_start):
             line = new_line(start,width,margin_top=2) # 2px spacing between lines
             line_count += 1 #count the number of lines on top of each other
-            if start<
         else:
             #6 comes from 4 (line height) + 2 (margin spacing) and 4 to remove single line spacing
+            # ... update start here ...
             line = new_line(start,width,margin_top=line_count*-6-4) # subtracted to reset to top
             line_count=0  # reset if the new line is non-overlapping
         
         lines+=line
-        previous_end=start+width
+        previous_end=end
     
     return template.replace("{line}",lines)
 
+def chr_slices(chr:pl.DataFrame)->np.ndarray:
+    previous_start = 0
+    previous_end = 0
+    ranges_list = []
+
+    overlap_range = [np.inf,np.ninf] #min,max
+
+    for (start,),(width,) in zip(chr.select("Start").iter_rows(),chr.select("Range").iter_rows()):
+        
+        end = start+width
+        #if no overlap, if -> (completely on left) and (completely on right)
+        if (start < previous_end) and (end>previous_start):
+            if start<overlap_range[0]:
+                overlap_range[0] = start
+            if end>overlap_range[1]:
+                overlap_range[1] = end
+        else:
+            ranges_list.append(overlap_range)
+            overlap_range = [np.inf,np.ninf] # reset overlap_range
+
+        previous_end=end
+    
+    slices  = np.array(ranges_list)
+    # ... update slices to reduce the spacing between neigboring slices 10px here...
+    return slices
+
+
 @timer
 def draw_chr(chr:pl.DataFrame,template:str)->None:
+    # ... update this function to draw multiple slices with same Y value ...
     width = chr.select("End").max()[0,0]
     chr_line = "\t"+CHROMOSOME.format(width)+"\n"
-
     return template.replace("{chr}",chr_line)
 
 def main()->None:
+    # ... this function also should be updated with new callings ...
     html = read_html() #template html file
 
     chr1 = read_data("chr1.csv")
